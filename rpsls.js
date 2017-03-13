b116ac3d8b04704d4c0af0839ca49189c642130d
Eisa Ahmed <eahmed234@gmail.com>
2017-03-13 14:21:05 +0000

    Update rpsls

--- rpsls.js
+++ rpsls.js
@@ -1,4 +1,27 @@
 function rpsls1 (form) {
   ui = form.rpslsResponse.value;
-  
+  var randNum = Math.floor(Math.random() * 5) + 1;
+  var comp = 0;
+  var human = 0;
+  if (ui.toUpperCase() === "ROCK") {
+    switch (randNum) {
+      case 1:
+        var p1 = "<p>It's a tie!</p>"
+        $("#rpslsForm").append(p1);
+        break;
+      
+      default:
+        // code
+    }
+  } else if (ui.toUpperCase() === "PAPER") {
+    
+  } else if (ui.toUpperCase() === "SCISSORS") {
+    
+  } else if (ui.toUpperCase() === "LIZARD") {
+    
+  } else if (ui.toUpperCase() === "SPOCK") {
+    
+  } else {
+    
+  }
 }
