34cdaa32c6c10914eba6ac1563fdbd7d5fb8e9c2
Eisa Ahmed <eahmed234@gmail.com>
2017-04-04 03:25:28 +0000

    fixed embarass

--- dragon.js
+++ dragon.js
@@ -5,6 +5,7 @@ var randnum = Math.floor(Math.random() * 2) + 1;
 function endGame(pNum,pAfter) {
   $(pNum).insertAfter(pAfter);
   document.getElementById("adventurenum").removeAttribute("onClick");
+  document.getElementById("adventurenum").setAttribute("onClick","resetFunction()");
 }
 
 function contGame(pNum,pAfter,nextAdventure) {
@@ -15,6 +16,12 @@ function contGame(pNum,pAfter,nextAdventure) {
 
 var password = "The one and only, Christopher Cannoli";
 
+function resetFunction(){
+  $("#adventureForm").empty();
+  document.getElementById("adventurenum").removeAttribute("onClick");
+  document.getElementById("adventurenum").setAttribute("onClick","adventure1(this.form)");
+}
+
 function adventure1 (form) {
     var ui = document.getElementById('response').value;
     $("#rpslsGame").remove();
@@ -22,10 +29,10 @@ function adventure1 (form) {
       var p1 = "<p id='p1'>Is it a gun or a sword?</p>";
       contGame(p1,"#init","adventure3(this.form)");
     } else if (ui.toUpperCase() === "NO") {
-      var p1 = "<p id='p1'>The dragon is approaching fast! (hit submit)</p>";
+      p1 = "<p id='p1'>The dragon is approaching fast! (hit submit)</p>";
       contGame(p1,"#init","adventure2(this.form)");
     } else {
-      var p1 = "<p>You are an uncooperative person. That is dead.</p>";
+      p1 = "<p>You are an uncooperative person. That is dead.</p>";
       endGame(p1,"#init");
     }
 }
@@ -37,7 +44,7 @@ function adventure2 (form) {
       var p2 = "<p>The dragon passes right past you and devours someone else. You breathe a sigh of relief and continue on your way.</p>";
       endGame(p2,"#p1");
     } else {
-      var p2 = "<p id='p2'>The dragon halts right in front of you, and asks for a password?! What is the password?</p>";
+      p2 = "<p id='p2'>The dragon halts right in front of you, and asks for a password?! What is the password?</p>";
       console.log("The password is: " + password);
       contGame(p2,"#p1","adventure4(this.form)");
     }
@@ -54,14 +61,14 @@ function adventure3 (form) {
       var p3 = "<p>You slay the dragon with your handy weapon, and continue along your merry way.</p>";
       endGame(p3,"#p1");
     } else {
-      var p3 = "<p>You are unlucky, for the dragon is unusually stong for you. You are now dead.</p>";
+      p3 = "<p>You are unlucky, for the dragon is unusually stong for you. You are now dead.</p>";
       endGame(p3,"#p1");
     }
   } else if (ui.toUpperCase() === "GUN") {
-    var p3 = "<p id='p3'>The dragon scoffs at your puny gun and charges at you. You fire off your first shot! (hit submit)</p>";
+    p3 = "<p id='p3'>The dragon scoffs at your puny gun and charges at you. You fire off your first shot! (hit submit)</p>";
     contGame(p3,"#p1","adventure5(this.form)");
   } else {
-    var p3 = "<p>You're unwillingness to answer has lead you to your demise. Pity.</p>";
+    p3 = "<p>You're unwillingness to answer has lead you to your demise. Pity.</p>";
     endGame(p3,"#p1");
   }
 }
@@ -73,11 +80,11 @@ function adventure4 (form) {
       var p4 = "<p>The dragon accepts and lets you go on your way.</p>";
       endGame(p4,"#p2");
     } else {
-      var p4 = "<p>The dragon was joking about accepting any password, even though it was correct. You are now lunch.</p>";
+      p4 = "<p>The dragon was joking about accepting any password, even though it was correct. You are now lunch.</p>";
       endGame(p4,"#p2");
     }
   } else {
-    var p4 = "<p>The dragon laughs at your feeble attempt to guess the password before chowing down.</p>";
+    p4 = "<p>The dragon laughs at your feeble attempt to guess the password before chowing down.</p>";
     endGame(p4,"#p2");
   }
 }
@@ -96,12 +103,12 @@ function adventure5 (form) {
         document.getElementById("adventurenum").removeAttribute("onClick");
         break;
       } else if (health === 0) {
-        var p4end = "<p>You died. The end.</p>";
+        p4end = "<p>You died. The end.</p>";
         $("#adventureForm").append(p4end);
         document.getElementById("adventurenum").removeAttribute("onClick");
         break;
       } else if (health === 0 && dragHealth === 0) {
-        var p4end = "<p>The dragon died, but unfortunately you do too. Pity.</p>";
+        p4end = "<p>The dragon died, but unfortunately you do too. Pity.</p>";
         $("#adventureForm").append(p4end);
         document.getElementById("adventurenum").removeAttribute("onClick");
         break;
@@ -111,27 +118,27 @@ function adventure5 (form) {
             dragHealth -= 1;
             var p4 = "<p>You hit the Dragon! The dragon has " + dragHealth + " lives left</p>";
             $("#adventureForm").append(p4);
-            var rand = Math.floor(Math.random() * 4) + 1;
+            rand = Math.floor(Math.random() * 4) + 1;
             break;
           case 2:
             health -= 1;
-            var p4 = "<p>You missed the Dragon, and he hits you! You have " + health + " lives left</p>";
+            p4 = "<p>You missed the Dragon, and he hits you! You have " + health + " lives left</p>";
             $("#adventureForm").append(p4);
-            var rand = Math.floor(Math.random() * 4) + 1;
+            rand = Math.floor(Math.random() * 4) + 1;
             break;
           case 3:
             dragHealth -= 1;
             health -= 1;
-            var p4 = "<p>You hit the Dragon, but the dragon also gets a swipe at you!! The dragon has " + dragHealth + " lives left, and you have " + health + " lives left!</p>";
+            p4 = "<p>You hit the Dragon, but the dragon also gets a swipe at you!! The dragon has " + dragHealth + " lives left, and you have " + health + " lives left!</p>";
             $("#adventureForm").append(p4);
-            var rand = Math.floor(Math.random() * 4) + 1;
+            rand = Math.floor(Math.random() * 4) + 1;
             break;
           case 4:
             dragHealth -= 1;
             health -= 1;
-            var p4 = "<p>You hit the dragon, but he does damage to you also! The dragon has " + dragHealth + " lives left, and you have " + health + " lives left!</p>";
+            p4 = "<p>You hit the dragon, but he does damage to you also! The dragon has " + dragHealth + " lives left, and you have " + health + " lives left!</p>";
             $("#adventureForm").append(p4);
-            var rand = Math.floor(Math.random() * 4) + 1;
+            rand = Math.floor(Math.random() * 4) + 1;
             break;
           default:
             console.log("But y");
@@ -142,7 +149,7 @@ function adventure5 (form) {
     }
   } else {
     console.log("But y tho?");
-    var p5 = "<p>But y tho?</p>";
+    p5 = "<p>But y tho?</p>";
     endGame(p5,"#p3");
   }
 }
--- rpsls.js
+++ rpsls.js
@@ -1,75 +1,17 @@
 /*global $*/
 
-var result;
-var comp = 0;
-var humanScore = 0;
-var randNum;
-var compChoice;
-
-function randFunction() {
-  randNum = Math.floor(Math.random() * 11) + 1;
-  switch (randNum) {
-    case 1:
-      compChoice = "rock";
-      break;
-    case 2:
-      compChoice = "fire";
-      break;
-    case 3:
-      compChoice = "scissors";
-      break;
-    case 4:
-      compChoice = "human";
-      break;
-    case 5:
-      compChoice = "wolf";
-      break;
-    case 6:
-      compChoice = "sponge";
-      break;
-    case 7:
-      compChoice = "paper";
-      break;
-    case 8:
-      compChoice = "air";
-      break;
-    case 9:
-      compChoice = "water";
-      break;
-    case 10:
-      compChoice = "devil";
-      break;
-    case 11:
-      compChoice = "gun";
-      break;
-    default:
-      console.log("Why?!");
-  }
+function eventListner() {
+  $('#rpslsResponse').keypress(function(e) {
+    var code = e.keyCode || e.which;
+    if (code === 13) {
+      $("#rpslsBtn").click();
+    }
+  });
 }
 
-function humanWin(against) {
-  humanScore += 1;
-  randNum = Math.floor(Math.random() * 5) + 1;
-  result = "<p>You win against " + against + "! Comp: " + comp + " You: " + humanScore + "</p>";
-}
+$(document).ready(function(){eventListner()});
 
-function compWin(against) {
-  comp += 1;
-  randNum = Math.floor(Math.random() * 5) + 1;
-  result = "<p>Computer wins with " + against + "! Comp: " + comp + " You: " + humanScore + "</p>";
-}
-
-function tie() {
-  randNum = Math.floor(Math.random() * 5) + 1;
-  result = "<p>It's a tie! Comp: " + comp + " You: " + humanScore + "</p>";
-}
-
-function invalid() {
-  randNum = Math.floor(Math.random() * 5) + 1;
-  result = "<p>Invalid input! Comp: " + comp + " You: " + humanScore + "</p>";
-}
-
-function rpsObjects(rock,fire,scissors,human,wolf,sponge,paper,air,water,devil,gun) {
+function rpsObjects(rock, fire, scissors, human, wolf, sponge, paper, air, water, devil, gun, name) {
   this.rock = rock;
   this.fire = fire;
   this.scissors = scissors;
@@ -81,135 +23,85 @@ function rpsObjects(rock,fire,scissors,human,wolf,sponge,paper,air,water,devil,g
   this.water = water;
   this.devil = devil;
   this.gun = gun;
+  this.name = name;
 }
 
-var rock = new rpsObjects("tie","win","win","win","win","win","lose","lose","lose","lose","lose");
-var fire = new rpsObjects("lose","tie","win","win","win","win","win","lose","lose","lose","lose");
-var scissors = new rpsObjects("lose","lose","tie","win","win","win","win","win","lose","lose","lose");
-var human = new rpsObjects("lose","lose","lose","tie","win","win","win","win","win","lose","lose");
-var wolf = new rpsObjects("lose","lose","lose","lose","tie","win","win","win","win","win","lose");
-var sponge = new rpsObjects("lose","lose","lose","lose","lose","tie","win","win","win","win","win");
-var paper = new rpsObjects("win","lose","lose","lose","lose","lose","tie","win","win","win","win");
-var air = new rpsObjects("win","win","lose","lose","lose","lose","lose","tie","win","win","win");
-var water = new rpsObjects("win","win","win","lose","lose","lose","lose","lose","tie","win","win");
-var devil = new rpsObjects("win","win","win","win","lose","lose","lose","lose","lose","tie","win");
-var gun = new rpsObjects("win","win","win","win","win","lose","lose","lose","lose","lose","tie");
+var rock = new rpsObjects("tie", "lose", "lose", "lose", "lose", "lose", "wins", "wins", "wins", "wins", "wins", "rock");
+var fire = new rpsObjects("wins", "tie", "lose", "lose", "lose", "lose", "lose", "wins", "wins", "wins", "wins", "fire");
+var scissors = new rpsObjects("wins", "wins", "tie", "lose", "lose", "lose", "lose", "lose", "wins", "wins", "wins", "scissors");
+var human = new rpsObjects("wins", "wins", "wins", "tie", "lose", "lose", "lose", "lose", "lose", "wins", "wins", "human");
+var wolf = new rpsObjects("wins", "wins", "wins", "wins", "tie", "lose", "lose", "lose", "lose", "lose", "wins", "wolf");
+var sponge = new rpsObjects("wins", "wins", "wins", "wins", "wins", "tie", "lose", "lose", "lose", "lose", "lose", "sponge");
+var paper = new rpsObjects("lose", "wins", "wins", "wins", "wins", "wins", "tie", "lose", "lose", "lose", "lose", "paper");
+var air = new rpsObjects("lose", "lose", "wins", "wins", "wins", "wins", "wins", "tie", "lose", "lose", "lose", "air");
+var water = new rpsObjects("lose", "lose", "lose", "wins", "wins", "wins", "wins", "wins", "tie", "lose", "lose", "water");
+var devil = new rpsObjects("lose", "lose", "lose", "lose", "wins", "wins", "wins", "wins", "wins", "tie", "lose", "devil");
+var gun = new rpsObjects("lose", "lose", "lose", "lose", "lose", "wins", "wins", "wins", "wins", "wins", "tie", "gun");
 
+var rpsArray = [rock, fire, scissors, human, wolf, sponge, paper, air, water, devil, gun];
+
+var result;
+var comp = 0;
+var humanScore = 0;
+var randNum = Math.floor(Math.random() * 11);
+var compChoice = rpsArray[randNum];
+
+function humanWin() {
+  humanScore += 1;
+  result = "<p>You win against " + compChoice.name + "! Comp: " + comp + " You: " + humanScore + "</p>";
+  randNum = Math.floor(Math.random() * 11);
+  compChoice = rpsArray[randNum];
+}
+
+function compWin() {
+  comp += 1;
+  result = "<p>Computer wins with " + compChoice.name + "! Comp: " + comp + " You: " + humanScore + "</p>";
+  randNum = Math.floor(Math.random() * 11);
+  compChoice = rpsArray[randNum];
+}
+
+function tie() {
+  randNum = Math.floor(Math.random() * 11);
+  compChoice = rpsArray[randNum];
+  result = "<p>It's a tie! Comp: " + comp + " You: " + humanScore + "</p>";
+}
+
+function invalid() {
+  randNum = Math.floor(Math.random() * 11);
+  compChoice = rpsArray[randNum];
+  result = "<p>Invalid input! Comp: " + comp + " You: " + humanScore + "</p>";
+}
 
 function compareFunction() {
   var ui = $("#rpslsResponse").val();
   var humanChoice = ui.toLowerCase();
-  randFunction();
-  if (humanChoice === compChoice){
+  if (compChoice.name === humanChoice) {
     tie();
-    console.log("Actual tie");
-  } else if (humanChoice === "rock"){
-    if (rock[compChoice] === "win") {
-      humanWin(compChoice);
-    } else if (rock[compChoice] === "lose"){
-      compWin(compChoice);
-    } else {
-      console.log("hittin rock bottom");
-    }
-  } else if (humanChoice === "fire"){
-    if (fire[compChoice] === "win") {
-      humanWin(compChoice);
-    } else if (fire[compChoice] === "lose"){
-      compWin(compChoice);
-    } else {
-      console.log("hittin rock bottom");
-    }
-  } else if (humanChoice === "scissors"){
-    if (scissors[compChoice] === "win") {
-      humanWin(compChoice);
-    } else if (scissors[compChoice] === "lose"){
-      compWin(compChoice);
-    } else {
-      console.log("hittin rock bottom");
-    }
-  } else if (humanChoice === "human"){
-    if (human[compChoice] === "win") {
-      humanWin(compChoice);
-    } else if (human[compChoice] === "lose"){
-      compWin(compChoice);
-    } else {
-      console.log("hittin rock bottom");
-    }
-  } else if (humanChoice === "wolf"){
-    if (wolf[compChoice] === "win") {
-      humanWin(compChoice);
-    } else if (wolf[compChoice] === "lose"){
-      compWin(compChoice);
-    } else {
-      console.log("hittin rock bottom");
-    }
-  } else if (humanChoice === "sponge"){
-    if (sponge[compChoice] === "win") {
-      humanWin(compChoice);
-    } else if (sponge[compChoice] === "lose"){
-      compWin(compChoice);
-    } else {
-      console.log("hittin rock bottom");
-    }
-  } else if (humanChoice === "paper"){
-    if (paper[compChoice] === "win") {
-      humanWin(compChoice);
-    } else if (paper[compChoice] === "lose"){
-      compWin(compChoice);
-    } else {
-      console.log("hittin rock bottom");
-    }
-  } else if (humanChoice === "air"){
-    if (air[compChoice] === "win") {
-      humanWin(compChoice);
-    } else if (air[compChoice] === "lose"){
-      compWin(compChoice);
-    } else {
-      console.log("hittin rock bottom");
-    }
-  } else if (humanChoice === "water"){
-    if (water[compChoice] === "win") {
-      humanWin(compChoice);
-    } else if (water[compChoice] === "lose"){
-      compWin(compChoice);
-    } else {
-      console.log("hittin rock bottom");
-    }
-  } else if (humanChoice === "devil"){
-    if (devil[compChoice] === "win") {
-      humanWin(compChoice);
-    } else if (devil[compChoice] === "lose"){
-      compWin(compChoice);
-    } else {
-      console.log("hittin rock bottom");
-    }
-  } else if (humanChoice === "gun"){
-    if (gun[compChoice] === "win") {
-      humanWin(compChoice);
-    } else if (gun[compChoice] === "lose"){
-      compWin(compChoice);
-    } else {
-      console.log("hittin rock bottom");
-    }
-  } else {
+  }
+  else if (compChoice[humanChoice] === "lose") {
+    compWin();
+  }
+  else if (compChoice[humanChoice] == "wins") {
+    humanWin();
+  }
+  else {
     invalid();
   }
 }
 
-function resetBtn(){
+function resetBtn() {
   document.getElementById("rpslsBtn").removeAttribute("onClick");
-  document.getElementById("rpslsBtn").setAttribute("onClick","resetFunction()");
+  document.getElementById("rpslsBtn").setAttribute("onClick", "resetFunction()");
   $("#rpslsBtn").html("Reset?");
-  
 }
 
-function resetFunction(){
+function resetFunction() {
   $("#rpslsForm").empty();
   comp = 0;
   humanScore = 0;
   result = "";
   document.getElementById("rpslsBtn").removeAttribute("onClick");
-  document.getElementById("rpslsBtn").setAttribute("onClick","rpsls1(this.form)");
+  document.getElementById("rpslsBtn").setAttribute("onClick", "rpsls1(this.form)");
   $("#rpslsBtn").html("Submit");
 }
 
@@ -219,17 +111,14 @@ function rpsls1(form) {
     result = "<p>You win best of 5!</p>";
     resetBtn();
     $("#rpslsForm").append(result);
-    console.log("human win " + result);
   }
   else if (comp === 5) {
     result = "<p>Computer wins best of 5!</p>";
     resetBtn();
     $("#rpslsForm").append(result);
-    console.log("comp win " + result);
   }
   else {
     compareFunction();
     $("#rpslsForm").append(result);
-    console.log("bottom " + result);
   }
 }
