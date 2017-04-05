be29b287de90d99490ad024030b96569c1104f02
Eisa Ahmed <eahmed234@gmail.com>
2017-04-05 03:33:10 +0000

    seperation and colooooors

--- rpsls.js
+++ rpsls.js
@@ -2,60 +2,65 @@
 
 function eventListner() {
   $('#rpslsResponse').keypress(function(e) {
-    var code = e.keyCode || e.which;
+    let code = e.keyCode || e.which;
     if (code === 13) {
       $("#rpslsBtn").click();
     }
   });
 }
 
-$(document).ready(function(){eventListner()});
+$(document).ready(function() {
+  $("#rpslsGame").hide();
+  eventListner();
+});
 
-function rpsObjects(rock, fire, scissors, human, wolf, sponge, paper, air, water, devil, gun, name) {
-  this.rock = rock;
-  this.fire = fire;
-  this.scissors = scissors;
-  this.human = human;
-  this.wolf = wolf;
-  this.sponge = sponge;
-  this.paper = paper;
-  this.air = air;
-  this.water = water;
-  this.devil = devil;
-  this.gun = gun;
-  this.name = name;
+class rpsObjects {
+  constructor(rock, fire, scissors, human, wolf, sponge, paper, air, water, devil, gun, name) {
+    this.rock = rock;
+    this.fire = fire;
+    this.scissors = scissors;
+    this.human = human;
+    this.wolf = wolf;
+    this.sponge = sponge;
+    this.paper = paper;
+    this.air = air;
+    this.water = water;
+    this.devil = devil;
+    this.gun = gun;
+    this.name = name;
+  }
 }
 
-var rock = new rpsObjects("tie", "lose", "lose", "lose", "lose", "lose", "wins", "wins", "wins", "wins", "wins", "rock");
-var fire = new rpsObjects("wins", "tie", "lose", "lose", "lose", "lose", "lose", "wins", "wins", "wins", "wins", "fire");
-var scissors = new rpsObjects("wins", "wins", "tie", "lose", "lose", "lose", "lose", "lose", "wins", "wins", "wins", "scissors");
-var human = new rpsObjects("wins", "wins", "wins", "tie", "lose", "lose", "lose", "lose", "lose", "wins", "wins", "human");
-var wolf = new rpsObjects("wins", "wins", "wins", "wins", "tie", "lose", "lose", "lose", "lose", "lose", "wins", "wolf");
-var sponge = new rpsObjects("wins", "wins", "wins", "wins", "wins", "tie", "lose", "lose", "lose", "lose", "lose", "sponge");
-var paper = new rpsObjects("lose", "wins", "wins", "wins", "wins", "wins", "tie", "lose", "lose", "lose", "lose", "paper");
-var air = new rpsObjects("lose", "lose", "wins", "wins", "wins", "wins", "wins", "tie", "lose", "lose", "lose", "air");
-var water = new rpsObjects("lose", "lose", "lose", "wins", "wins", "wins", "wins", "wins", "tie", "lose", "lose", "water");
-var devil = new rpsObjects("lose", "lose", "lose", "lose", "wins", "wins", "wins", "wins", "wins", "tie", "lose", "devil");
-var gun = new rpsObjects("lose", "lose", "lose", "lose", "lose", "wins", "wins", "wins", "wins", "wins", "tie", "gun");
+let rock = new rpsObjects("tie", "lose", "lose", "lose", "lose", "lose", "wins", "wins", "wins", "wins", "wins", "rock");
+let fire = new rpsObjects("wins", "tie", "lose", "lose", "lose", "lose", "lose", "wins", "wins", "wins", "wins", "fire");
+let scissors = new rpsObjects("wins", "wins", "tie", "lose", "lose", "lose", "lose", "lose", "wins", "wins", "wins", "scissors");
+let human = new rpsObjects("wins", "wins", "wins", "tie", "lose", "lose", "lose", "lose", "lose", "wins", "wins", "human");
+let wolf = new rpsObjects("wins", "wins", "wins", "wins", "tie", "lose", "lose", "lose", "lose", "lose", "wins", "wolf");
+let sponge = new rpsObjects("wins", "wins", "wins", "wins", "wins", "tie", "lose", "lose", "lose", "lose", "lose", "sponge");
+let paper = new rpsObjects("lose", "wins", "wins", "wins", "wins", "wins", "tie", "lose", "lose", "lose", "lose", "paper");
+let air = new rpsObjects("lose", "lose", "wins", "wins", "wins", "wins", "wins", "tie", "lose", "lose", "lose", "air");
+let water = new rpsObjects("lose", "lose", "lose", "wins", "wins", "wins", "wins", "wins", "tie", "lose", "lose", "water");
+let devil = new rpsObjects("lose", "lose", "lose", "lose", "wins", "wins", "wins", "wins", "wins", "tie", "lose", "devil");
+let gun = new rpsObjects("lose", "lose", "lose", "lose", "lose", "wins", "wins", "wins", "wins", "wins", "tie", "gun");
 
-var rpsArray = [rock, fire, scissors, human, wolf, sponge, paper, air, water, devil, gun];
+let rpsArray = [rock, fire, scissors, human, wolf, sponge, paper, air, water, devil, gun];
 
-var result;
-var comp = 0;
-var humanScore = 0;
-var randNum = Math.floor(Math.random() * 11);
-var compChoice = rpsArray[randNum];
+let result;
+let comp = 0;
+let humanScore = 0;
+let randNum = Math.floor(Math.random() * 11);
+let compChoice = rpsArray[randNum];
 
 function humanWin() {
   humanScore += 1;
-  result = "<p>You win against " + compChoice.name + "! Comp: " + comp + " You: " + humanScore + "</p>";
+  result = `<p>You win against ${compChoice.name}! Comp: ${comp} You: ${humanScore}</p>`;
   randNum = Math.floor(Math.random() * 11);
   compChoice = rpsArray[randNum];
 }
 
 function compWin() {
   comp += 1;
-  result = "<p>Computer wins with " + compChoice.name + "! Comp: " + comp + " You: " + humanScore + "</p>";
+  result = `<p>Computer wins with ${compChoice.name}! Comp: ${comp} You: ${humanScore}</p>`;
   randNum = Math.floor(Math.random() * 11);
   compChoice = rpsArray[randNum];
 }
@@ -63,18 +68,18 @@ function compWin() {
 function tie() {
   randNum = Math.floor(Math.random() * 11);
   compChoice = rpsArray[randNum];
-  result = "<p>It's a tie! Comp: " + comp + " You: " + humanScore + "</p>";
+  result = `<p>It's a tie! Comp: ${comp} You: ${humanScore}</p>`;
 }
 
 function invalid() {
   randNum = Math.floor(Math.random() * 11);
   compChoice = rpsArray[randNum];
-  result = "<p>Invalid input! Comp: " + comp + " You: " + humanScore + "</p>";
+  result = `<p>Invalid input! Comp: ${comp} You: ${humanScore}</p>`;
 }
 
 function compareFunction() {
-  var ui = $("#rpslsResponse").val();
-  var humanChoice = ui.toLowerCase();
+  let ui = $("#rpslsResponse").val();
+  let humanChoice = ui.toLowerCase();
   if (compChoice.name === humanChoice) {
     tie();
   }
@@ -87,6 +92,11 @@ function compareFunction() {
   else {
     invalid();
   }
+}
+
+function initialScreen(){
+  $("#initScreen").fadeOut();
+  $("#rpslsGame").fadeIn();
 }
 
 function resetBtn() {
@@ -106,7 +116,6 @@ function resetFunction() {
 }
 
 function rpsls1(form) {
-  $("#rules").fadeOut();
   if (humanScore === 5) {
     result = "<p>You win best of 5!</p>";
     resetBtn();
